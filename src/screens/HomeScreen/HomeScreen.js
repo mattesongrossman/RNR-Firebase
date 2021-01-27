59a61ed18e2acabf37cc03f0a7e09cbd9eefd1df
Matteson Grossman <mattesongrossman@gmail.com>
2021-01-27 15:32:33 -0500

    Login works

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 
-export default function HomeScreen(props) {
+export default function HomeScreen(props, { navigation }) {
   return (
     <View>
       <Text>Home Screen</Text>
+      <Button
+        title="Update the title"
+        onPress={() => navigation.setOptions({ title: "Updated!" })}
+      />
     </View>
   );
 }
